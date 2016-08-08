067c0a8b9e73a111ddce278cf5b2ab2ffd8b43d8
ASmallBoy <lanou@taoyajie.local>
2016-08-08 15:27:33 +0800

    轮播图

---
+++ 定时器轮播图封装/定时器轮播图使用方式/定时器轮播图使用方式/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  定时器轮播图使用方式
+//
+//  Created by I三生有幸I on 16/5/27.
+//  Copyright © 2016年 盛辰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
